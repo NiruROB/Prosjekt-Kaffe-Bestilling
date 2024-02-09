8cb5b553ec0a61d1e9e756d868d20816cde373df
NiruROB <141549550+NiruROB@users.noreply.github.com>
2024-02-09 12:49:45 +0100

    Add project files.

---
+++ Prosjekt Kaffe Bestilling/Cafe.cs
@@ -0,0 +1,69 @@
+﻿using Prosjekt_Kaffe_Bestilling;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prosjekt_Kaffe_Bestilling
+{
+
+    public class Cafe
+    {
+        private List<Product> menu = new List<Product>();
+        private Dictionary<String, int> order = new Dictionary<String, int>();
+
+        public void AddProduct(Product product)
+        {
+            menu.Add(product);
+        }
+        public void AddOrder(string productName, int quantity)
+        {
+
+            order[productName] = quantity;
+        }
+
+        public void RemoveOrder(string productsName)
+        {
+            order.Remove(productsName);
+
+        }
+        public void Menu()
+        {
+            foreach (var product in menu)
+            {
+                Console.WriteLine($"{product.Name}: {product.Price} kr");
+            }
+        }
+
+        public void ValueOrder()
+        {
+            foreach (var item in order)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+        }
+        public void OrderConfirmation()
+        {
+            double totalCost = 0;
+            Console.WriteLine("reciept");
+            foreach (var item in order)
+            {
+                Product product = menu.Find(p => p.Name == item.Key);
+                if (product != null)
+                {
+                    double itemsCost = item.Value * product.Price;
+                    totalCost += itemsCost;
+                    Console.WriteLine($"{item.Key}: {product.Price} x {item.Value} kr = {itemsCost} kr");
+
+                }
+                Console.WriteLine($"Total price: {totalCost}kr");
+
+            }
+            Console.WriteLine($"Total price: {totalCost} kr");
+
+        }
+
+    }
+}
---
+++ Prosjekt Kaffe Bestilling/Program.cs
@@ -0,0 +1,50 @@
+﻿using Prosjekt_Kaffe_Bestilling;
+using System;
+using System.Collections.Generic;
+
+namespace CafeOrderingSystem
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cafe cafe = new Cafe();
+
+            
+            cafe.AddProduct(new Product("Kaffe", 25));
+            cafe.AddProduct(new Product("Croissant", 40));
+            cafe.AddProduct(new Product("Tea", 30));
+            cafe.AddProduct(new Product("Bagel", 35));
+            cafe.AddProduct(new Product("Donut", 12));
+            cafe.AddProduct(new Product("Biscuit", 5)); 
+
+            Console.WriteLine("Welcome to the Cafe!!!!");
+            Console.WriteLine("Menu: ");
+            cafe.Menu();
+
+            cafe.AddOrder("Kaffe", 2);
+            cafe.AddOrder("Bagel", 3);
+            cafe.AddOrder("Croissant", 2);
+            cafe.AddOrder("Biscuit", 4); 
+
+            Console.WriteLine("\nYour Order: ");
+            cafe.ValueOrder();
+
+
+            cafe.RemoveOrder("Croissant");
+
+            Console.WriteLine("\nNew order list:");
+            cafe.ValueOrder();
+
+            // confirmation
+
+            cafe.OrderConfirmation();
+
+        }
+    }
+}
+   
+
+   
